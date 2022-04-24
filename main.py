06e6e664748e600d33cbb5f92350b702f46d324c
Johnny Dang <mail@jydago.com>
2022-04-24 22:30:09 +0200

    Renamed raw tweet downloader

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 from loguru import logger
 
-from twitter_extractor import main_raw_download_tweets, main_process_tweets, main_database_ingest_tweets
+from twitter_extractor import main_download_raw_tweets, main_process_tweets, main_database_ingest_tweets
 
 if __name__ == '__main__':
     logger.info("Started pipeline")
     load_dotenv()
 
-    main_raw_download_tweets.main()
+    main_download_raw_tweets.main()
     main_process_tweets.main()
     main_database_ingest_tweets.main()
 
